04a5045f45c85653b969e8395bcda7a0b925bd94
Renato Yufra <renato.esis@gmail.com>
2017-10-09 13:11:48 -0500

    Configurando Webpack

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
   <template lang="pug">
   #app
-    img(src='https://renatoyufra.github.io/platzimusic/dist/logo.png')
+    img(src='dist/logo.png')
     h1 PlatziMusic
     select(v-model="selectedCountry")
       option(v-for="country in countries" v-bind:value="country.value") {{ country.name }}
